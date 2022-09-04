a660c5d02057a52d621ddc1c005f6c439d6ac3b3
zhuxiaowei <1649201921@qq.com>
2022-09-04 08:36:44 +0800

    first commit

---
+++ config/routes.ts
@@ -0,0 +1,54 @@
+﻿export default [
+  {
+    path: '/user',
+    layout: false,
+    routes: [
+      {
+        name: 'login',
+        path: '/user/login',
+        component: './user/Login',
+      },
+      {
+        component: './404',
+      },
+    ],
+  },
+  {
+    path: '/welcome',
+    name: 'welcome',
+    icon: 'smile',
+    component: './Welcome',
+  },
+  {
+    path: '/admin/sub-page',
+    name: 'admin',
+    icon: 'crown',
+    access: 'canAdmin',
+    component: './TableList',
+    // routes: [
+    //   {
+    //     path: '/admin/sub-page',
+    //     name: 'sub-page',
+    //     icon: 'smile',
+    //     component: './Welcome',
+    //   },
+    //   {
+    //     component: './404',
+    //   },
+    // ],
+  },
+  {
+    name: 'list.table-list',
+    icon: 'table',
+    path: '/list',
+    access: 'canAdmin',
+    component: './TableList',
+  },
+  {
+    path: '/',
+    redirect: '/welcome',
+  },
+  {
+    component: './404',
+  },
+];
